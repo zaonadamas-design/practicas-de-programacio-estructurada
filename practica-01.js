15e6f5f5451bdaaf6bb98ac18e70fc151635fcf2
zaonadamas <zaonadamas@gmail.com>
2026-08-19 13:35:08 -0600

    practica

---
+++ practica-01.js
@@ -0,0 +1,12 @@
+let num1 = 10;
+let num2 = 20;
+
+let suma = num1 + num2;
+let resta = num1 - num2;
+let multiplicacion = num1 * num2;
+let modulo = num1 % num2;
+
+console.log('la suma es: ' + suma);
+console.log('la resta es: ' + resta);
+console.log('la multiplicacion es: ' + multiplicacion);
+console.log('el modulo es: ' + modulo);
